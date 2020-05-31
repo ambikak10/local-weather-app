5e3fa4a20caf53674762ca4c5fc9d0e547000815
Ambika K <ambikak10@gmail.com>
2020-05-30 22:27:41 -0700

    test files added

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,8 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { ClimateService } from './climate.service';
+import { ClimateFakeService } from './climate.fake.sevice';
 
 
 describe('AppComponent', () => {
@@ -9,6 +11,10 @@ describe('AppComponent', () => {
       imports: [
         RouterTestingModule,
    
+      ],
+      providers: [
+        {provide: ClimateService, useClass:
+        ClimateFakeService}
       ],
       declarations: [
         AppComponent
--- src/app/city-search/city-search.component.spec.ts
+++ src/app/city-search/city-search.component.spec.ts
@@ -3,7 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { CitySearchComponent } from './city-search.component';
 import { ClimateService } from '../climate.service';
 import { ClimateFakeService } from '../climate.fake.sevice';
-import { FormControl } from '@angular/forms';
+
 
 describe('CitySearchComponent', () => {
   let component: CitySearchComponent;

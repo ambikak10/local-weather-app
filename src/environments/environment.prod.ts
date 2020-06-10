5fcf784227851b57b37785fa558b9d91756ca81f
Ambika K <ambikak10@gmail.com>
2020-06-09 21:02:47 -0700

    environmwnt prod.ts modified

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
-};
+         production: true,
+        
+         baseUrl: 'http://',
+         appID: '56fff8aecc220e9a18c040f1746da42f',
+       };
